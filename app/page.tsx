726a9430eff88cf1a70d9d5d90e4d301b1b42315
Chrismaganga <chrismaganga3@gmail.com>
2024-02-18 16:48:21 +0200

    update

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Example from "./components/Example"
+
+
+const page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Example/>
+    </div>
+  )
+}
+
+export default page
